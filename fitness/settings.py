dd79ec58d623a0681e86a870244585f4663db75b
PWashburn55 <pw325555@gmail.com>
2020-12-30 15:26:13 +0000

    hard-code allowed_hosts settings

--- fitness/settings.py
+++ fitness/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", [])
+#ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", [])
+ALLOWED_HOSTS = ['fitness-that-fits.herokuapp.com', 'localhost']
 
 
 # Application definition
