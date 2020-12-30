913b8cdbeeaa7a8d2987bcaa469c55ab87fdbed7
PWashburn55 <pw325555@gmail.com>
2020-12-30 09:16:04 +0000

    added profiles template

--- checkout/admin.py
+++ checkout/admin.py
@@ -4,29 +4,31 @@ from .models import Order, OrderLineItem
 
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
-    readonly_fields = ('lineitem_total',)
+    #readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'date',
-                       'shipping', 'order_total',
-                       'grand_total', 'original_bag', 
-                       'stripe_pid')
+   # readonly_fields = ('order_number', 'date',
+                      # 'delivery_cost', 'shipping', 'order_total',
+                       #'grand_total', 'original_bag',
+                      # 'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county', 'shipping',
-              'order_total', 'grand_total', 'original_bag', 
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total', 'original_bag',
               'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
-                    'order_total', 'shipping',
+                    'order_total', 'delivery_cost',
                     'grand_total',)
 
     ordering = ('-date',)
 
 
 admin.site.register(Order, OrderAdmin)
+
+
