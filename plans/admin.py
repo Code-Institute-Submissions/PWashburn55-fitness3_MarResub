451f690bf0ed332b8c751ecfa15fc928661f842d
PWashburn55 <pw325555@gmail.com>
2020-12-31 17:55:43 +0000

    added views + plans template

--- plans/admin.py
+++ plans/admin.py
@@ -2,4 +2,24 @@ from django.contrib import admin
 from .models import Plan
 
 
-admin.site.register(Plan)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'description',
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'rating',
+    )
+
+    order = ('sku',)
+
+
+admin.site.register(Plan, PlanAdmin)
--- plans/views.py
+++ plans/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Plan
 
 # Create your views here.
+
+def all_plans(request):
+    """ A view to show all plans, including search queries """
+
+    plans = Plan.objects.all()
+
+    context = {
+        'plans': plans,
+    }
+
+    return render(request, 'plans/plans.html', context)
