843b582506282bd1578097405022bd1a9ebe9be6
PWashburn55 <pw325555@gmail.com>
2021-01-13 15:24:47 +0000

    changed footer

--- plans/views.py
+++ plans/views.py
@@ -1,16 +1,46 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Plan
-
-# Create your views here.
+from django.db.models import Q
+from django.db.models.functions import Lower
 
 
 def all_plans(request):
-    """ A view to show all plans, including search queries """
+    """ A view to show all plans, including sorting and search queries """
 
     plans = Plan.objects.all()
+    query = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                plans = plans.annotate(lower_name=Lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            plans = plans.order_by(sortkey)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No search criteria entered")
+                return redirect(reverse('plans'))
+
+            queries = Q(name__icontains=query) | \
+                Q(description__icontains=query)
+            plans = plans.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'plans': plans,
+        'search_term': query,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'plans/plans.html', context)
@@ -26,3 +56,16 @@ def plan_detail(request, plan_id):
     }
 
     return render(request, 'plans/plan_detail.html', context)
+
+"""
+def all_plans(request):
+     A view to show all plans, including search queries 
+
+    plans = Plan.objects.all()
+
+    context = {
+        'plans': plans,
+    }
+
+    return render(request, 'plans/plans.html', context)
+"""
