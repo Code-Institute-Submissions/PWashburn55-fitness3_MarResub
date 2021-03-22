81d6f1773cf6fa304fd6a486385d3dffa494adcd
PWashburn55 <pw325555@gmail.com>
2021-03-22 10:47:46 +0000

    changed more email settings

--- fitness/settings.py
+++ fitness/settings.py
@@ -195,3 +195,6 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    
+   
+
